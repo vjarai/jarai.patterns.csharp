60b161d3ebf2975d6b2e934b9ae2fbe517edb38f
Viktor <vjarai@users.noreply.github.com>
2022-10-07 20:55:13 +0200

    Servicelocator

---
+++ ServiceLocator.AntiPattern/ServiceLocator.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceLocator.AntiPattern
+{
+    /// <summary>
+    /// Servicelocator ANTI Pattern
+    /// Warum Antipattern: Testbarkeit, Mockbarkeit, unsichtbare Abhängigkeiten.
+    /// </summary>
+    public class ServiceLocator
+    {
+        public static T Resolve<T>() where T : class, new()
+        {
+            return new T();
+        }
+    }
+}

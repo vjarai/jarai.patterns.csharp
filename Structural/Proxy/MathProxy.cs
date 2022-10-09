b81b66678889bfdab389afd9e6f108c2ae9dd864
Viktor <viktor@nospam.org>
2022-10-09 11:33:20 +0200

    Proxy

---
+++ Structural/Proxy/MathProxy.cs
@@ -0,0 +1,33 @@
+﻿namespace Jarai.Patterns.Structural.Proxy
+{
+    /// <summary>
+    /// The 'Proxy Object' class
+    /// </summary>
+
+    public class MathProxy : IMath
+    {
+        private readonly Math _math;
+
+        public MathProxy(Math math)
+        {
+            _math = math;
+        }
+
+        public double Add(double x, double y)
+        {
+            return _math.Add(x, y);
+        }
+        public double Sub(double x, double y)
+        {
+            return _math.Sub(x, y);
+        }
+        public double Mul(double x, double y)
+        {
+            return _math.Mul(x, y);
+        }
+        public double Div(double x, double y)
+        {
+            return _math.Div(x, y);
+        }
+    }
+}
--- Structural/Proxy/Program.cs
+++ Structural/Proxy/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Jarai.Patterns.Structural.Proxy
+﻿using System;
+
+namespace Jarai.Patterns.Structural.Proxy
 {
-    internal class Program
+    /// <summary>
+    /// Proxy Design Pattern
+    /// </summary>
+
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            // Create math proxy
+
+            var mathProxy = new MathProxy(new Math());
+
+            // Do the math
+
+            Console.WriteLine("4 + 2 = " + mathProxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + mathProxy.Sub(4, 2));
+            Console.WriteLine("4 * 2 = " + mathProxy.Mul(4, 2));
+            Console.WriteLine("4 / 2 = " + mathProxy.Div(4, 2));
+
+            // Wait for user
+
+            Console.ReadKey();
         }
     }
 }

3a0df07a7e2f19f16bf755370a58e1936ab6db49
Viktor <viktor@nospam.org>
2023-09-12 20:57:00 +0200

    .

---
+++ NullObject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NullObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLogger = new Logger();
+            myLogger.Write("Hallo");
+
+            myLogger = new NullLogger();
+            myLogger.Write("Welt");
+
+        }
+    }
+}
---
+++ NullObject/_ReadMe/_ReadMe.md
@@ -0,0 +1,5 @@
+# Null Object
+
+The Null Object pattern allows an Method to be a empty operation.
+
+Frequency of use: medium-low
